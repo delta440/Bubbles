f4231a6f84f0129877a957953d3cc7724a103d40
Jay <darthdudey@gmail.com>
2013-06-01 18:33:15 -0600

    half finished edit customer by phone

--- editcustomer.php
+++ editcustomer.php
@@ -1,5 +1,15 @@
 <html><head><title>Edit Customer</title></head></title>
 
+<?php if(isset($_POST['searchphone'])){
+include('sqlconnect.php');
+mysql_query("USE bubbles");
+$PhoneNumber = $_POST['phonenumber'];
+$query = "SELECT CustomerID FROM PhoneNumbers WHERE PhoneNumber = '$PhoneNumber'";
+$result = mysql_query($query) or die('Query"' . $query . '" failed' . mysql_error());
+$row = mysql_fetch_array($result);
+$CustomerID = $row['CustomerID'];
+
+?>
 
 
 
